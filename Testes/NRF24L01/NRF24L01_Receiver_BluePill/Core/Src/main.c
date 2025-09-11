69e5bcb518409fb1a4634d78bb3a71625785e30b
LeBaggett <130565050+LeBaggett@users.noreply.github.com>
2025-09-11 19:49:22 -0300

    Atualização de Receptor para enviar via UART se foi recebido e o dado enviado, e tambem um arquivo do bmp180

--- Testes/NRF24L01/NRF24L01_Receiver_BluePill/Core/Src/main.c
+++ Testes/NRF24L01/NRF24L01_Receiver_BluePill/Core/Src/main.c
@@ -23,15 +23,17 @@
 /* USER CODE BEGIN Includes */
 
 #include "NRF24L01.h"
+#include <stdio.h>
+#include <string.h>
 
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
+/*
 long map(long value, long fromLow, long fromHigh, long toLow, long toHigh) {
   return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
-}
+}*/
 
 /* USER CODE END PTD */
 
@@ -50,13 +52,14 @@ SPI_HandleTypeDef hspi1;
 
 TIM_HandleTypeDef htim1;
 
-NRF24_HandleTypeDef nrf;
-
-uint8_t data_received;
-
-uint8_t dc;
+UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
+
+NRF24_HandleTypeDef nrf;
+
+uint8_t data_received = -1;
+uint8_t ok = -1;
 
 /* USER CODE END PV */
 
@@ -65,6 +68,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_TIM1_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -105,6 +109,7 @@ int main(void)
   MX_GPIO_Init();
   MX_SPI1_Init();
   MX_TIM1_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
 	nrf.hspi = &hspi1;
@@ -112,7 +117,7 @@ int main(void)
 	nrf.CSN_port = CSN_GPIO_Port; nrf.CSN_pin = CSN_Pin;
 
 	NRF24_Init_RX(&nrf);
-	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+	/*HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);*/
 
   /* USER CODE END 2 */
 
@@ -122,14 +127,21 @@ int main(void)
   {
     /* USER CODE END WHILE */
 
-	  uint8_t ok = NRF24_Receive(&nrf, &data_received, sizeof(data_received));
-	  	  	  	   NRF24_DebugLED(LED_BUILTIN_GPIO_Port, LED_BUILTIN_Pin, ok);
-	  	  	  	   HAL_Delay(1000);
-
-	  dc = map(data_received, 0, 4095, 0, 500);
-	  TIM1->CCR1=dc;
-
     /* USER CODE BEGIN 3 */
+
+	  ok = NRF24_Receive(&nrf, &data_received, sizeof(data_received));
+	  	  	  	  	   NRF24_DebugLED(LED_BUILTIN_GPIO_Port, LED_BUILTIN_Pin, ok);
+	  	  	  	  	   HAL_Delay(1000);
+
+	 char msg[100];  // buffer para armazenar a string formatada
+
+	 // Mostra se recebeu ou não
+	 sprintf(msg, "OK: %d | Data Received: %d \r\n", ok, data_received);
+	 HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+
+
+	  	  /*dc = map(data_received, 0, 4095, 0, 500);
+	  	  TIM1->CCR1=dc;*/
   }
   /* USER CODE END 3 */
 }
@@ -270,6 +282,39 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 2 */
   HAL_TIM_MspPostInit(&htim1);
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
 
 }
 
